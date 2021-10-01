720103386a258e80b74dcdeb828cc257c6d1db6e
USER <hameed.momin@gmail.com>
2021-10-01 11:15:58 +0530

    ansible

---
+++ launch/launch.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+COMPONENT=$1
+
+# -z validating
+
+if [ -z "${COMPONENT}" ]; then
+  echo "Component Input is Needed"
+  exit 1
+fi
+
+LID=lt-0b970bef7d9f13f52
+LVER=2
+
+## Validating the instance is already there
+
+DNS_UPDATE() {
+  PRIVATEIP=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id Z05483541JD3IOXLSP16I --change-batch file:///tmp/record.json | jq
+}
+
+INSTANCE_CREATE() {
+  INSTANCE_STATE=$(aws --region us-east-1 ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
+  if [ "${INSTANCE_STATE}" = "running" ]; then
+    echo "${COMPONENT} Instance already exists"
+    DNS_UPDATE
+    return 0
+  fi
+
+  if [ "${INSTANCE_STATE}" = "stopped" ]; then
+    echo "${COMPONENT} Instance already exists!!"
+    return 0
+  fi
+
+  echo -n Instance ${COMPONENT} created - IPADDRESS is
+  aws --region us-east-1 ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq | grep  PrivateIpAddress  |xargs -n1
+  sleep 30
+  DNS_UPDATE
+
+}
+
+if [ "${1}" == "all" ]; then
+  for component in frontend mongodb catalogue redis user cart mysql shipping rabitmq payment ; do
+    COMPONENT=$component
+    INSTANCE_CREATE
+  done
+else
+  COMPONENT=$1
+  INSTANCE_CREATE
+fi
